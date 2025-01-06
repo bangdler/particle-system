8b3e3a4067dd7e49d580e42394134a9939bd5475
bangdler <zbthz90@gmail.com>
2025-01-07 00:33:02 +0900

    refact: 예제 페이지 초기 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,47 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { fire } from './basicModel';
+import { createParticleEmitter } from './model';
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//   </div>
+// `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document
+  .querySelector<HTMLDivElement>('#app')!
+  .appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  renderer.render(scene, camera);
+  controls.update();
+}
+renderer.setAnimationLoop(animate);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+
+const fireParticle = createParticleEmitter(fire);
+console.log(fireParticle);
+fireParticle.init();
+// fireParticle.run();
+scene.add(fireParticle.particleObject.mesh);
