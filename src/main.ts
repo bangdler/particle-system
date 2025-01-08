7923497eed13a38edf2ddf38181ed8f27c90e5e1
bangdler <zbthz90@gmail.com>
2025-01-08 22:30:19 +0900

    feat: github link 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css';
 import { initControls } from './controls';
 import { initScene } from './scene';
+import { initGithubLink } from './githubLink';
 
 initScene();
 initControls();
+initGithubLink();
